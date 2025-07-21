50a555fce1fab096af507569ed9cf9d331aaa86d
prajwal <prajwalkreddy17@gmail.com>
2025-07-21 14:19:53 +0530

    FE

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,74 +1,56 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { app, BrowserWindow } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+
+let mainWindow: BrowserWindow;
 
 function createWindow(): void {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
-    show: false,
-    autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+  mainWindow = new BrowserWindow({
+    show: false, // Hide initially for smoother load
+    autoHideMenuBar: false,
+    title: '', // Vite can override this
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+      sandbox: false,
+      nodeIntegration: false,
+      contextIsolation: true,
     }
-  })
+  });
 
-  mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
+  mainWindow.once('ready-to-show', () => {
+    setTimeout(() => {
+      mainWindow.maximize()
+    }, (100));
+    mainWindow.show();
+  });
 
-  mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
-
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
+  // Electron-renderer-url is injected by the electron-vite tool
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('demo'); // windows specific, a unique identifier for windows
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  // Enable F12, disable Cmd/Ctrl+R in production
   app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
+    optimizer.watchWindowShortcuts(window);
+  });
 
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
+  createWindow();
 
-  createWindow()
+  // macOS: Reopen window when dock icon is clicked
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed (except macOS)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+});
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+  useEffect(() => {
+    console.log("window", window);
+    console.log("window electron", window.electron);
+    console.log("window api", window.api);
+  }, [window]);
+
+  const [todo, setTodo] = useState("");
+
+  function handleAddTodo() {}
+
   return (
     <>
-      <div>
-        <div>Hello world</div>
-        <div>Todo Crud app</div>
+      <div className="min-h-screen m-auto h-full w-full flex items-center justify-center">
+        <div className="border-2 px-6 bg-[#f1f5f9] w-full max-w-lg py-5 space-y-3 rounded-xl">
+          <h1 className="text-3xl font-bold">Todo App</h1>
+          <div className="text-md text-neutral-500">A simple todo app</div>
+          <div className="space-x-3 flex flex-col  w-full justify-center gap-3">
+            <label htmlFor="addtodo" className="font-semibold">
+              Add Todo:
+            </label>
+            <input
+              type="text"
+              value={todo}
+              onChange={(e) => setTodo(e.target.value)}
+              className="border-2 px-2 py-2 rounded-lg w-full"
+            />
+            <button
+              className="border-none outline-none bg-black py-2 rounded-xl px-2  text-white"
+              onClick={handleAddTodo}
+            >
+              Add todo
+            </button>
+          </div>
+          <div className="w-full text-center text-lg font-medium">
+            All Todos:
+          </div>
+        </div>
       </div>
     </>
   );
